1d3817c01e2d8485673be3636bcda9670b634902
Rafffan <vanidrisraffan@gmail.com>
2024-09-18 19:51:53 +0700

    finish

--- src/app/api/enrollments/route.ts
+++ src/app/api/enrollments/route.ts
@@ -3,7 +3,7 @@ import {
   zEnrollmentPostBody, 
   zEnrollmentDeleteBody 
 } from "@lib/schema";
-import { DB, Student } from "@lib/DB";
+import { DB} from "@lib/DB";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (request:NextRequest) => {
@@ -27,14 +27,16 @@ export const GET = async (request:NextRequest) => {
 
   //check if user provide one of 'studentId' or 'courseNo'
   //User must not provide both values, and must not provide nothing
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Please provide either studentId or courseNo and not both!",
-  //   },
-  //   { status: 400 }
-  // );
+  if ((!studentId&&!courseNo) || (studentId&&courseNo)) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Please provide either studentId or courseNo and not both!",
+      },
+      { status: 400 }
+    );
+  }
+  
 
   //get all courses enrolled by a student
   if (studentId) {
@@ -60,10 +62,17 @@ export const GET = async (request:NextRequest) => {
     const studentIdList = [];
     for (const enroll of DB.enrollments) {
       //your code here
+      if (enroll.courseNo === courseNo) {
+        studentIdList.push(enroll.studentId);
+      }
     }
 
-    const students:Student[] = [];
+    const students = [];
     //your code here
+    for (const studentId of studentIdList) {
+      const student = DB.students.find((x) => x.studentId === studentId);
+      students.push(student);
+    }
 
     return NextResponse.json({
       ok: true,
@@ -141,21 +150,28 @@ export const DELETE = async (request:NextRequest) => {
   const { studentId, courseNo } = body;
 
   //check if studentId and courseNo exist on enrollment
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Enrollment does not exist",
-  //   },
-  //   { status: 404 }
-  // );
-
+  const foundStudent = DB.enrollments.find((x) => x.studentId === studentId);
+  const foundCourse = DB.enrollments.find((x) => x.courseNo === courseNo);
+  
+  if (!foundStudent || !foundCourse) {
+  return NextResponse.json(
+    {
+      ok: false,
+      message: "Enrollment does not exist",
+    },
+    { status: 404 }
+  );
+  }
   //perform deletion by using splice or array filter
+
+  DB.enrollments=DB.enrollments.filter((x)=> x.studentId !== studentId || x.courseNo !== courseNo);
+  // const foundEnroll = DB.enrollments.findIndex((x) => x.courseNo === courseNo && x.studentId === studentId); 
+  // DB.enrollments.splice(foundEnroll,1);
 
   //if code reach here it means deletion is complete
   return NextResponse.json({
-    ok: true,
-    message: "Enrollment has been deleted",
+  ok: true,
+  message: "Enrollment has been deleted",
   });
+  
 };
-
